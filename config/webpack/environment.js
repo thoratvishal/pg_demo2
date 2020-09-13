a70e8f09b7c386dd57fa9f4a0e504dcc4745e9b2
vishalthorat <vishal.thorat@quickinsure.co.in>
2020-09-13 20:04:41 +0530

    environment.js is implemented

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,12 @@
 const { environment } = require('@rails/webpacker')
+const webpack = require('webpack')
+
+environment.plugins.append('Provide', new webpack.ProvidePlugin({
+  $: 'jquery',
+  jQuery: 'jquery',
+  jquery: 'jquery/src/jquery',
+  Popper: ['popper.js', 'default'],
+  moment: 'moment/moment'
+}))
 
 module.exports = environment
